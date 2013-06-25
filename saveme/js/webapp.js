74e31928ec9d15a897dd23c0e7ca5680d923f60b
Stan Cristinel <stan.cristinel1990@gmail.com>
2013-06-25 18:54:34 +0200

    SaveMe App - Firefox OS Workshop at L'Aquila (3rd place)

---
+++ saveme/js/webapp.js
@@ -0,0 +1,29 @@
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + 'Ý <br>Longitude is ' + longitude + 'Ý</p>';
+
+    var img = new Image();
+    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";        
+    output.appendChild(img);    
+    window.open('mailto:your.mail@example.com?subject=SaveMe Location&body=Latitude: ' + latitude + ' | Longitude: ' + longitude);
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+    
+}
